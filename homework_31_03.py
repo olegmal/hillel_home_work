e8d421b2f0a767a3e778a1ec8fec54263dfd45ff
olegmal <olegm.ua@gmail.com>
2023-04-03 20:27:21 +0300

    revised homework

--- homework_31_03.py
+++ homework_31_03.py
@@ -5,22 +5,39 @@
 # в один і повертає. В будь - якому іншому випадку - функція повертає
 # кортеж з двох агрументів.
 
-# print замість  return, щоб виводило в консоль
+
+
+# def test(a, b):
+#     try:
+#         print(a * b)
+#         return a* b
+
+#     except:
+#         print(a + b)
+#         return a + b
+
+#     else:
+#         print(a, b)
+
+
+# test(3.4, 6.4)
+
 
 def test(a, b):
-    try:
+    if type(a) and type(b) == int or type(a) and type(b) == float:
         print(a * b)
-        return a* b
-
-    except:
+        return a * b
+    elif isinstance(a, str) and isinstance(b, str):
         print(a + b)
         return a + b
-
     else:
         print(a, b)
+        return a, b
 
 
-test(3.4, 6.4)
+test("Hi ","World") 
+
+
 
 
 # 2. Візьміть код з заняття і доопрацюйте натупним
@@ -34,47 +51,47 @@ test(3.4, 6.4)
 #
 # User == AI
 
-from random import randint
+# from random import randint
 
-def get_ai_number():
-    number = randint(1, 10)
-    print(f'AI: {number}')
-    return number
-
-
-def get_user_number():
-    while True:
-        try:
-            return  int(input('Enter the number: '))
-        except ValueError:
-            print('Number pls!')
-
-def check_numbers(ai_number, user_number):
-    result = ai_number == user_number
-    if result:
-        print(f'Result is, {result}')
-        return result
-    elif (ai_number - user_number) > 10:
-        print('Холодно!')
-    elif 5 < (ai_number - user_number) < 10:
-        print("Тепло!")
-    elif (ai_number - user_number) < 5:
-        print('Гаряче!')
+# def get_ai_number():
+#     number = randint(1, 10)
+#     print(f'AI: {number}')
+#     return number
 
 
-def game_guess_number():
-    print("Game begins!")
-    ai_number = get_ai_number()
-    user_tries = int(input("how many tries do you need?: "))
-    count = 1
-    while count <= user_tries:
-        user_number = get_user_number()
-        is_game_end = check_numbers(ai_number, user_number)
-        count += 1
-        if is_game_end:
-            break
-        print("Wrong, try again!")
-print("user wins!")
+# def get_user_number():
+#     while True:
+#         try:
+#             return  int(input('Enter the number: '))
+#         except ValueError:
+#             print('Number pls!')
 
-game_guess_number()
+# def check_numbers(ai_number, user_number):
+#     result = ai_number == user_number
+#     if result:
+#         print(f'Result is, {result}')
+#         return result
+#     elif (ai_number - user_number) > 10:
+#         print('Холодно!')
+#     elif 5 < (ai_number - user_number) < 10:
+#         print("Тепло!")
+#     elif (ai_number - user_number) < 5:
+#         print('Гаряче!')
+
+
+# def game_guess_number():
+#     print("Game begins!")
+#     ai_number = get_ai_number()
+#     user_tries = int(input("how many tries do you need?: "))
+#     count = 1
+#     while count <= user_tries:
+#         user_number = get_user_number()
+#         is_game_end = check_numbers(ai_number, user_number)
+#         count += 1
+#         if is_game_end:
+#             break
+#         print("Wrong, try again!")
+# print("user wins!")
+
+# game_guess_number()
 
